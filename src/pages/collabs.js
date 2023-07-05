41e3db94e3de835882738152e3fb0157d431f9a9
TetraXP <john@tetraxp.com>
2023-07-05 18:54:17 -0400

    Collab page additions and fixes

--- src/pages/collabs.js
+++ src/pages/collabs.js
@@ -6,17 +6,51 @@ import { ReactSVG } from "react-svg";
 import catalyst from "../assets/Catalyst.webp";
 import reaperwolf from "../assets/Wolfgang.webp";
 import purgatory from "../assets/Purgatory.webp";
+import bornchosen from "../images/art/BORN CHOSEN.jpeg";
+import goodriddance from "../images/art/Good Riddance.jpeg";
+import ineverymotion from "../images/art/In Every Motion.jpeg";
 import { Link } from "gatsby";
 
 const breakpointColumnsObj = {
-  default: 6,
+  default: 5,
   1536: 5,
   1280: 4,
-  1024: 4,
-  768: 4,
-  640: 4,
+  1024: 3,
+  768: 2,
+  640: 2,
 };
-
+const images = [
+  {
+    link: "https://gallery.manifold.xyz/0x8478bd23db7a701bfc4f6175b565edce6ad1167d/37",
+    alt: "Catalyst (2023)",
+    src: catalyst,
+  },
+  {
+    link: "https://opensea.io/assets/ethereum/0x259808462d8c56dd252da349cfc2dd82330ed3e4/3",
+    alt: "ReaperWolf (2023)",
+    src: reaperwolf,
+  },
+  {
+    link: "https://opensea.io/assets/ethereum/0x259808462d8c56dd252da349cfc2dd82330ed3e4/6",
+    alt: "Purgatory (2023)",
+    src: purgatory,
+  },
+  {
+    link: "https://foundation.app/@TETRA/tetraworld/10",
+    alt: "Born Chosen (2022)",
+    src: bornchosen,
+  },
+  {
+    link: "https://foundation.app/@MakingIt247/makingit/115",
+    alt: "Good Riddance (2023)",
+    src: goodriddance,
+  },
+  {
+    alt: "Good Riddance (2023)",
+    src: ineverymotion,
+  },
+  // add more images here
+];
 const CollabsPage = () => {
 
   return (
@@ -51,42 +85,22 @@ const CollabsPage = () => {
       <div>
 
       <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="flex justify-center gap-1"
-          columnClassName="grid_column gap-8"
-        >
-          <a
-            href="https://gallery.manifold.xyz/0x8478bd23db7a701bfc4f6175b565edce6ad1167d/37"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:brightness-125 duration-300 h-auto max-w-full"
-          >
-                    <img
-            src={catalyst}
-            alt={`Catalyst (2023)`}
-            
-          /></a>
-          <a
-            href="https://opensea.io/assets/ethereum/0x259808462d8c56dd252da349cfc2dd82330ed3e4/3"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:brightness-125 duration-300 h-auto max-w-full"
-          >
-                              <img
-            src={reaperwolf}
-            alt={`ReaperWolf (2023)`}
-          /></a>
-          <a
-            href="https://opensea.io/assets/ethereum/0x259808462d8c56dd252da349cfc2dd82330ed3e4/6"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:brightness-125 duration-300 h-auto max-w-full"
-          >
-          <img
-            src={purgatory}
-            alt={`Purgatory (2023)`}
-          /></a>
-        </Masonry>
+  breakpointCols={breakpointColumnsObj}
+  className="flex justify-center gap-1"
+>
+  {images.map((image, index) => (
+    <div key={index} className="grid mb-1">
+      <a
+        href={image.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:brightness-125 duration-300 h-auto max-w-full"
+      >
+        <img src={image.src} alt={image.alt} />
+      </a>
+    </div>
+  ))}
+</Masonry>
       </div>
     </motion.div>
   );
